db954b88f9b61b626548fa6dc5e39146e3b9d1a5
davidhorst <davidmichaelhorst@gmail.com>
2017-02-09 21:05:27 -0500

    drf permissions

--- server/settings/base.py
+++ server/settings/base.py
@@ -88,12 +88,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        # 'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        # 'rest_framework.authentication.SessionAuthentication',
-        # 'rest_framework.authentication.BasicAuthentication',
-        # 'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
 }
 
